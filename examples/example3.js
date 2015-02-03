d2de5221e4f21e916486d79edf8749f63616ff9c
Roydon DSouza <roydon@techjini.com>
2015-02-04 02:41:44 +0530

    more changes - stable

--- examples/example3.js
+++ examples/example3.js
@@ -1,8 +1,8 @@
 var tasks = [
-{"startDate":new Date("Sun Dec 25 00:00:01 EST 2012"),"endDate":new Date("Sun Jan 08 23:59:59 EST 2013"),"taskName":"Sierra","status":"RUNNING","name":"Campaign Name - X34234 34530453"},
-{"startDate":new Date("Sun Dec 20 00:00:01 EST 2012"),"endDate":new Date("Sun Jan 10 23:59:59 EST 2013"),"taskName":"TWC Auto","status":"FAILED","name":"TWC Auto 2345234 234234SDFS"},
-{"startDate":new Date("Sun Dec 20 00:00:01 EST 2012"),"endDate":new Date("Sun Jan 10 23:59:59 EST 2013"),"taskName":"TWC ","status":"FAILED","name":"TWC Auto 2345234 234234SDFS"},
-{"startDate":new Date("Sun Jan 01 00:00:01 EST 2013"),"endDate":new Date("Sun Jan 05 23:59:59 EST 2013"),"taskName":"TWC ","status":"FAILED","name":"TWC Auto 2345234 234234SDFS"}
+{"startDate":new Date("Tue Dec 25 00:00:01 EST 2012"),"endDate":new Date("Tue Jan 08 23:59:59 EST 2013"),"taskName":"Sierra","status":"RUNNING","name":"Campaign Name - X34234 34530453"},
+{"startDate":new Date("Thu Dec 20 00:00:01 EST 2012"),"endDate":new Date("Thu Jan 10 23:59:59 EST 2013"),"taskName":"TWC","status":"FAILED","name":"TWC Auto 2345234 234234SDFS"},
+{"startDate":new Date("Thu Dec 20 00:00:01 EST 2012"),"endDate":new Date("Thu Jan 10 23:59:59 EST 2013"),"taskName":"TWC","status":"FAILED","name":"TWC Auto 2345234 234234SDFS"},
+{"startDate":new Date("Tue Jan 01 00:00:01 EST 2013"),"endDate":new Date("Sat Jan 05 23:59:59 EST 2013"),"taskName":"TWC","status":"FAILED","name":"TWC Auto 2345234 234234SDFS"}
 ];
 
 var taskStatus = {
@@ -12,7 +12,7 @@ var taskStatus = {
     "KILLED" : "bar-killed"
 };
 
-var taskNames = [ "TWC ","TWC", "Corolla", "Sierra", "Piedmont Eye Care", "TWC Auto" ];
+var taskNames = [ "TWC", "Corolla", "Sierra", "Piedmont Eye Care" ];
 
 tasks.sort(function(a, b) {
     return a.endDate - b.endDate;
@@ -23,7 +23,7 @@ tasks.sort(function(a, b) {
 });
 var minDate = tasks[0].startDate;
 
-var format = "%d %b";
+var format = "%d";
 var timeDomainString = "1week";
 
 var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);//.height(450).width(800);;
@@ -64,12 +64,12 @@ function changeTimeDomain(timeDomainString) {
 	break;
 
     case "1week":
-	format = "%d %b";
+	format = "%d";
 	gantt.timeDomain([ d3.time.day.offset(getEndDate(), -7), getEndDate() ]);
 	break;
 
      case "1month":
-    format = "%d %b";
+    format = "%d";
     gantt.timeDomain([ d3.time.day.offset(getEndDate(), -7), d3.time.day.offset(getEndDate(),+50 ) ]);
     break;
     default:
@@ -89,14 +89,14 @@ function getEndDate() {
     return lastEndDate;
 }
 function prev(){
-    format = "%d %b";
+    format = "%d";
     gantt.timeDomain([ d3.time.day.offset(getEndDate(), -7), getEndDate() ]);
     gantt.tickFormat(format);
     gantt.redraw(tasks);
 
 }
 function next(){
-    format = "%d %b";
+    format = "%d";
     gantt.timeDomain([ getEndDate(), d3.time.day.offset(getEndDate(), +7) ]);
     gantt.tickFormat(format);
     gantt.redraw(tasks);
@@ -109,12 +109,14 @@ function addTask() {
     var taskStatusKeys = Object.keys(taskStatus);
     var taskStatusName = taskStatusKeys[Math.floor(Math.random() * taskStatusKeys.length)];
     var taskName = taskNames[Math.floor(Math.random() * taskNames.length)];
+     var name = "test";
 
     tasks.push({
-	"startDate" : d3.time.hour.offset(lastEndDate, Math.ceil(90)),
-	"endDate" : d3.time.hour.offset(lastEndDate, (Math.ceil(1003)) + 1),
+	"startDate" : d3.time.day.offset(lastEndDate, Math.ceil(Math.random(10))),
+	"endDate" : d3.time.day.offset(lastEndDate, (Math.ceil(Math.random(74))) + 1),
 	"taskName" : taskName,
-	"status" : taskStatusName
+	"status" : taskStatusName,
+    "name": name
     });
 
     changeTimeDomain(timeDomainString);
--- gantt-chart-d3.js
+++ gantt-chart-d3.js
@@ -17,22 +17,24 @@ d3.gantt = function() {
 		bottom : 20,
 		left : 150
     };
-    var timeDomainStart = d3.time.day.offset(new Date(),-3);
-    var timeDomainEnd = d3.time.hour.offset(new Date(),+3);
+    var timeDomainStart = d3.time.day.offset(new Date(),-300);
+    var timeDomainEnd = d3.time.hour.offset(new Date(),+300);
     var timeDomainMode = FIT_TIME_DOMAIN_MODE;// fixed or fit
     var taskTypes = [];
     var taskStatus = [];
     var height = document.body.clientHeight - margin.top - margin.bottom-5;
     var width = document.body.clientWidth - margin.right - margin.left-5;
 
-    var tickFormat = "%d %b";
+    var tickFormat = "%d"; //%b
      
 
     var keyFunction = function(d) {
+    	console.log("key"+d.startDate);
 		return d.startDate + d.taskName + d.endDate;
     };
 
     var rectTransform = function(d) {
+    	console.log("transform"+d);
 		return "translate(" + x(d.startDate) + "," + y(d.taskName) + ")";
     };
 
@@ -46,7 +48,7 @@ d3.gantt = function() {
 	    .tickSize(height-margin.top,10)
 	    .tickPadding(-15);
 
-    var yAxis = d3.svg.axis().scale(y).orient("right").tickSize(0);
+    var yAxis = d3.svg.axis().scale(y).orient("right").tickFormat("").tickSize(0);
 
     var initTimeDomain = function(tasks) {
 	if (timeDomainMode === FIT_TIME_DOMAIN_MODE) {
@@ -77,7 +79,7 @@ d3.gantt = function() {
 			.tickSize(height-margin.top,10)
 			.tickPadding(-15); //modified from 8
 
-		yAxis = d3.svg.axis().scale(y).orient("right").tickSize(0);
+		yAxis = d3.svg.axis().scale(y).orient("right").tickFormat("").tickSize(0);
     };
     
     function gantt(tasks) {
@@ -96,29 +98,29 @@ d3.gantt = function() {
 	.attr("height", height + margin.top + margin.bottom)
 	.attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 	
-      svg.selectAll("g")
-	 .data(tasks, keyFunction).enter().append("g")
-	 .append("rect")
-	 .attr("rx", 0) //round borders
-         .attr("ry", 0) //round borders
-	 .attr("class", function(d){ //adding classname
-	     if(taskStatus[d.status] == null){ return "bar";}
-	     return taskStatus[d.status];
-	     }) 
-	 .attr("y", 0)
-	 .attr("transform", rectTransform)
-	 .attr("height", function(d) { return CAMPAIGN_HEIGHT;/*y.rangeBand();*/ })
-	 .attr("width", function(d) { 
-	     return (x(d.endDate) - x(d.startDate)); 
-	     })
-	 .on("click", function(){ alert('click captured'); });
+ //      svg.selectAll("g")
+	//  .data(tasks, keyFunction).enter().append("g").attr("class","node") //.attr("transform", rectTransform)
+	//  .append("rect")
+	//  .attr("rx", 0) //round borders
+ //         .attr("ry", 0) //round borders
+	//  .attr("class", function(d){ //adding classname
+	//      if(taskStatus[d.status] == null){ return "bar";}
+	//      return taskStatus[d.status];
+	//      }) 
+	//  .attr("y", 0)
+	//  .attr("transform", rectTransform)
+	//  .attr("height", function(d) { return CAMPAIGN_HEIGHT;y.rangeBand(); })
+	//  .attr("width", function(d) { 
+	//      return (x(d.endDate) - x(d.startDate)); 
+	//      })
+	//  .on("click", function(){ alert('click captured'); });
 
-	//EXTRA CUSTOM - TEXT ON RECTANGLE 
-	 svg.selectAll("g")
-		 .append('text').text(function(d){return d.name; })
-	                .attr('x', 10)
-	                .attr('y', function(d) { return /*y.rangeBand()*/CAMPAIGN_HEIGHT/2; })
-	                .attr('fill', 'black') .attr("transform", rectTransform);
+	// //EXTRA CUSTOM - TEXT ON RECTANGLE 
+	//  svg.selectAll("g")
+	// 	 .append('text').text(function(d){return d.name; })
+	//                 .attr('x', 10)
+	//                 .attr('y', function(d) { return /*y.rangeBand()*/CAMPAIGN_HEIGHT/2; })
+	//                 .attr('fill', 'black') .attr("transform", rectTransform);
 		 
 	 
 	 svg.append("g")
@@ -126,7 +128,7 @@ d3.gantt = function() {
 	 .attr("transform", "translate(0, " + (height - margin.top - margin.bottom) + ")")
 	 .transition()
 	 .call(xAxis)
-	 .selectAll(".tick text").attr("style","font-family:sans-serif;font-size:15pt;font-weight:bold").attr("x", function(d){  return 10 });
+	 .selectAll(".tick text").attr("style","font-family:sans-serif;font-size:12pt;").attr("x", function(d){  return 10 });
 	 
 	 svg.append("g").attr("class", "y axis").transition().call(yAxis);
 	 
@@ -142,146 +144,135 @@ d3.gantt = function() {
         var svg = d3.select("svg");
 
         var ganttChartGroup = svg.select(".gantt-chart");
- //        var rect = ganttChartGroup.selectAll("rect").data(tasks, keyFunction);
-        
- //        rect.enter()
- //         .insert("rect",":first-child")
- //         .attr("rx", 0) //border radius
- //         .attr("ry", 0) //border radius
-	//  .attr("class", function(d){ 
-	//      if(taskStatus[d.status] == null){ return "bar";}
-	//      return taskStatus[d.status];
-	//      }) 
-	//  .transition()
-	//  .attr("y", 0)
-	//  .attr("transform", rectTransform)
-	//  .attr("height", function(d) { return CAMPAIGN_HEIGHT;/*y.rangeBand();*/ })
-	//  .attr("width", function(d) { 
-	//      return (x(d.endDate) - x(d.startDate)); 
-	//      });
 
-	 
- //        rect.transition()
- //          .attr("transform", rectTransform)
-	//  .attr("height", function(d) { return CAMPAIGN_HEIGHT; }) //y.rangeBand()
-	//  .attr("width", function(d) { 
-	//      return (x(d.endDate) - x(d.startDate)); 
-	//      }); 
-
-        
-	// rect.exit().remove();
-
-	var rectData = ganttChartGroup.selectAll("rect").data(tasks, keyFunction).enter();
-	var rectGroup = rectData.append("g").attr("transform", rectTransform);
+		var rectData = ganttChartGroup.selectAll(".node").data(tasks, keyFunction);
+		var rect = rectData.enter();
+		var rectGroup = rect.append("g").attr("class","node").on("click", function(d){ alert('click captured'+d.name); });
 
 
-  	rectGroup.append("rect")
-  		.attr("id",function(d,i){ return "campaign-"+i;})
-  		.attr("class", function(d){ 
-	     	if(taskStatus[d.status] == null){ return "bar";}
-	     		return taskStatus[d.status];
-	     	}) 
-      	.attr("width", function(d) { 
-	      	return (x(d.endDate) - x(d.startDate)); 
-	      })
-      	.attr("height", function(d) { return CAMPAIGN_HEIGHT })
-      	.transition().duration(0)
+	  	rectGroup.append("rect")
+	  		.attr("title",function(d){ return "campaign";})
+	  		.attr("id",function(d,i){ return "campaign-"+i;})
+	  		.attr("class", function(d){ 
+		     	if(taskStatus[d.status] == null){ return "bar campaigns";}
+		     		return taskStatus[d.status]+" campaigns";
+		     	}) 
+	      	.attr("width", function(d) { 
+		      	return (x(d.endDate) - x(d.startDate)); 
+		      })
+	      	.attr("height", function(d) { return CAMPAIGN_HEIGHT })
+	      	.transition()
+	      		.attr("transform", rectTransform)
+	      	;
+
+		//top bar 
+			rectGroup.append("rect")
+				.attr("class","header")
+		  		.attr("fill", "green") 
+		      	.attr("width", function(d) { 
+			      	return (x(d.endDate) - x(d.startDate)); 
+			      })
+		      	.attr("height", 3)
+		      	.transition()
+		      		.attr("transform", rectTransform);
+
+		  	rectGroup.append("text")
+		  		.attr("class","campaigns_name")
+		      	.attr("x", 30)
+		      	.attr("y", CAMPAIGN_HEIGHT/2)
+		      	.attr("dy", ".35em")
+		      	.attr("style", function(d) {
+		        	if((x(d.endDate) - x(d.startDate))!=0){
+		        		return "";
+		        	} else {
+		        		return "display:none";
+		        	}
+		        })
+		      	.text(function(d) { return d.taskName; })
+		      	.transition()
+		      		.attr("transform", rectTransform);
+
+		      rectGroup.append("image")
+		      .attr("class","icon")
+		      .attr("x", CAMPAIGN_HEIGHT/3)
+		      .attr("y", CAMPAIGN_HEIGHT/5)
+		      .attr("height", CAMPAIGN_HEIGHT-10)
+		        .attr("width", function(d) {
+		        	if((x(d.endDate) - x(d.startDate))!=0){
+		        		return 15;
+		        	} else {
+		        		return 0;
+		        	}
+		        })
+		      .attr("xlink:href","https://qa-desk.collective.com/assets/statusWidget/active_icon-062230a04a602d1f17ea262b34a0198f.png")
+		      .transition()
+		      		.attr("transform", rectTransform)
+		      	;
+		      	
+      	//incase of a tooltip
+    //   	rectGroup.on("mouseover", function(d) {
+		  //     var g = d3.select(this); // The node
+		  //     // The class is used to remove the additional text later
+		  //     var info = g.append('text')
+		  //        .classed('info', true)
+		  //        .attr('x', 20)
+		  //        .attr('y', 10)
+		  //        .attr("transform", rectTransform)
+		  //        .text('More info');
+		  // })
+		  // .on("mouseout", function() {
+		  //     // Remove the info text on mouse out.
+		  //     d3.select(this).select('text.info').remove();
+		  // });
+
+  var node = ganttChartGroup.selectAll(".node").data(tasks, keyFunction);
+  var campaignBody = ganttChartGroup.selectAll(".campaigns").data(tasks, keyFunction);
+  var campaignText = ganttChartGroup.selectAll(".campaigns_name").data(tasks, keyFunction);
+  var campaignTopStroke = ganttChartGroup.selectAll(".header").data(tasks, keyFunction);
+   var campaignsStatusIcon = ganttChartGroup.selectAll(".icon").data(tasks, keyFunction);
+  
+
+
+
+	var translateVisualElements = function(a){
+		a.transition() 
+			.delay(0)
+  			.attr("transform", rectTransform)
+  			.attr("width", function(d) {
+    			return (x(d.endDate) - x(d.startDate));
+    		});		
+     };
+
+	var translateGraphicElements = function(a){
+		a.transition() .delay(0)
       		.attr("transform", rectTransform)
-      	;
+      		.attr("width", function(d) {
+	        	if((x(d.endDate) - x(d.startDate))!=0){
+	        		return 15;
+	        	} else {
+	        		return 0;
+	        	}
+        	})
+      		.attr("style", function(d) {
+	        	if((x(d.endDate) - x(d.startDate))>=40 ){
+	        		return "";
+	        	} else {
+	        		return "display:none";
+	        	}
+        	});
+     };
 
-//top bar 
-	rectGroup.append("rect")
-  		.attr("fill", "green") 
-      	.attr("width", function(d) { 
-	      	return (x(d.endDate) - x(d.startDate)); 
-	      })
-      	.attr("height", 3)
-      	.transition().duration(0)
-      		.attr("transform", rectTransform);
+	translateVisualElements(node);
+	translateVisualElements(campaignBody);
+	translateVisualElements(campaignTopStroke);
+	translateGraphicElements(campaignText);
+	translateGraphicElements(campaignsStatusIcon);
 
-  	rectGroup.append("text")
-      	.attr("x", 30)
-      	.attr("y", CAMPAIGN_HEIGHT/2)
-      	.attr("dy", ".35em")
-      	.attr("style", function(d) {
-        	if((x(d.endDate) - x(d.startDate))!=0){
-        		return "";
-        	} else {
-        		return "display:none";
-        	}
-        })
-      	.text(function(d) { return d.taskName; })
-      	.transition().duration(0)
-      		.attr("transform", rectTransform);
-
-      rectGroup.append("image")
-      .attr("x", CAMPAIGN_HEIGHT/3)
-      .attr("y", CAMPAIGN_HEIGHT/5)
-      .attr("height", CAMPAIGN_HEIGHT-10)
-        .attr("width", function(d) {
-        	if((x(d.endDate) - x(d.startDate))!=0){
-        		return 15;
-        	} else {
-        		return 0;
-        	}
-        })
-      .attr("xlink:href","https://qa-desk.collective.com/assets/statusWidget/active_icon-062230a04a602d1f17ea262b34a0198f.png")
-      .transition().duration(0)
-      		.attr("transform", rectTransform)
-      	;
-
-rectGroup.transition() .duration(0)
-.each(function() { d3.select(this.parentNode)
-      		.attr("transform", rectTransform)
-      	
-});
-
-
-// ganttChartGroup.selectAll("g").data(tasks, keyFunction).append("g").transition()
-// 	 //.each(function() { d3.select(this.parentNode)
-// 	 	.attr("transform", rectTransform);
-	 	//console.log(d3.select(this));
-	// });
-	 	//.attr("transform", rectTransform)
- 		// .attr("height", function(d) { return CAMPAIGN_HEIGHT; }) //y.rangeBand()
- 		// .attr("width", function(d) { 
-   //    		return (x(d.endDate) - x(d.startDate)); 
-   //    	}); 
-
-
-//console.log(rectGroup.transition());
-	//   ganttChartGroup.selectAll("g").data(tasks, keyFunction).transition()
-	// //.each(function() { d3.select(this.parentNode)
-	 	
-	//  	.attr("transform", rectTransform)
- // 		.attr("height", function(d) { return CAMPAIGN_HEIGHT; }) //y.rangeBand()
- // 		.attr("width", function(d) { 
- //      		return (x(d.endDate) - x(d.startDate)); 
- //      //	}); 
-	//   });
-	//  ganttChartGroup.selectAll("rect").data(tasks, keyFunction).transition()
-	
-	//  	.attr("transform", rectTransform)
- // 		.attr("height", function(d) { return CAMPAIGN_HEIGHT; }) //y.rangeBand()
- // 		.attr("width", function(d) { 
- //      		return (x(d.endDate) - x(d.startDate)); 
- 
-	//   });
-   //    	
-   //  rect.transition()
-   //    	.attr("transform", rectTransform)
- 		// .attr("height", function(d) { return CAMPAIGN_HEIGHT; }) //y.rangeBand()
- 		// .attr("width", function(d) { 
-   //   		return (x(d.endDate) - x(d.startDate)); 
-   //   	}); 
-
-      ganttChartGroup.selectAll("rect").data(tasks, keyFunction).exit().each(function() { d3.select(this.parentNode).remove(); });
-	//rectData.exit().each(function() { d3.select(this.parentNode).remove(); });
-
+	rectData.exit().remove();
 
 
 	svg.select(".x").transition().call(xAxis)
-		.selectAll(".tick text").attr("style","font-family:sans-serif;font-size:15pt;font-weight:bold").attr("x", function(d){  return 10 });
+		.selectAll(".tick text").attr("style","font-family:sans-serif;font-size:12pt").attr("x", function(d){  return 10 });
 	svg.select(".y").transition().call(yAxis);
 	
 	return gantt;
